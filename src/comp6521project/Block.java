0df81ee4b9c564eafa92bd7d82b746bd328dd129
Natt <Natt@192.168.2.36>
2021-02-24 21:16:00 -0500

    Block size is 2Kb not 1Kb

--- src/comp6521project/Block.java
+++ src/comp6521project/Block.java
@@ -4,7 +4,25 @@ import java.util.ArrayList;
 
 public class Block {
     public static final int recordsPerBlock = 9;
-    public static final int bytesPerBlock = 1024;
+    public static final int bytesPerBlock = 2 * Main.kiloByte;// 1024;
+    /**
+     * https://www.javamex.com/tutorials/memory/string_memory_usage.shtml
+     * the minimum memory usage of a Java String
+     * multiply the number of characters of the String by two;
+     * add 38;
+     * if the result is not a multiple of 8, round up to the next multiple of 8;
+     * the result is generally the minimum number of bytes taken up on the heap by the String.
+//          byte[] utf16Bytes;
+//    		try {
+//    			utf16Bytes = result.getBytes("UTF-16BE");
+//    			System.out.println(utf16Bytes.length);
+//    		} catch (UnsupportedEncodingException e) {
+//    			// TODO Auto-generated catch block
+//    			e.printStackTrace();
+//    		}
+     * public static final int bytesPerBlock = 2 * Main.kiloByte;
+     */
+    
     ArrayList<Tuple> records;
 
     public Block(){
